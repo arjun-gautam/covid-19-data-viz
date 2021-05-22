0e47eee591669fddf3d72169447b9ad45238592e
Arjun Gautam <arjun.gautam.1694@gmail.com>
2021-05-23 00:07:17 +0545

    added ga

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-178394506-1');
+ReactGA.pageview('Covid Page Hit!!');
 
 ReactDOM.render(
   <React.StrictMode>
